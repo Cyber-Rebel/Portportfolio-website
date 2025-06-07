96873cd19069c5565afeede4721320ffebefdc8a
Cyber-Rebel <np103177@gmail.com>
2025-06-07 21:02:41 +0530

    Part-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
-// import React from 'react'
-import Home from './page/Home.jsx'
-import React, { useState } from 'react';
-import Stact from './page/Stact.jsx';
-
+import React from 'react'
 
 const App = () => {
-
-
   return (
-    <div className=' w-full h-full  bg-black '>
-
-      <Home/>
-      <Stact/>
-    </div>
-  
-
+    <div>App</div>
   )
 }
-export default App;
 
-
-// gsap // scrolltrical // animation 
+export default App
