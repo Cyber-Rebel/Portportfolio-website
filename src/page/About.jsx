e4d97591774af85d0dc98a0c7048acef978238cb
Cyber-Rebel <np103177@gmail.com>
2025-09-05 13:08:37 +0530

    change

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,25 +1,34 @@
-import React from "react";
-import ContactForm from "../components/Contact.jsx";
-import ProfileSection from "../components/ProfileSection.jsx";
-import MeetingSection from "../components/MeetingSection.jsx";
+import React from 'react'
 
-const BookingPage = () => {
+const About = () => {
   return (
-    <div className="min-h-screen bg-black w-full text-white flex items-start justify-center p-10">
+    <div>About</div>
+  )
+}
+
+export default About
+// import React from "react";
+// import ContactForm from "../components/Contact.jsx";
+// import ProfileSection from "../components/ProfileSection.jsx";
+// import MeetingSection from "../components/MeetingSection.jsx";
+
+// const BookingPage = () => {
+//   return (
+//     <div className="min-h-screen bg-black w-full text-white flex items-start justify-center p-10">
     
       
-      <div className="w-full max-w-4xl space-y-10">
-        {/* Profile Section */}
-        <ProfileSection />
+//       <div className="w-full max-w-4xl space-y-10">
+//         {/* Profile Section */}
+//         <ProfileSection />
 
-        {/* Meeting Section */}
-        <MeetingSection />
+//         {/* Meeting Section */}
+//         <MeetingSection />
         
-        {/* Contact Form */}
-        <ContactForm />
-      </div>
-    </div>
-  );
-};
+//         {/* Contact Form */}
+//         <ContactForm />
+//       </div>
+//     </div>
+//   );
+// };
 
-export default BookingPage;
+// export default BookingPage;
